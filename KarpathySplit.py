af71acd26ad02b0a5434da006ff3d857e95e920c
Simeon Schüz <simeon.schuez@uni-jena.de>
2020-10-13 14:46:36 +0200

    use ids from Karpathy splits

--- KarpathySplit.py
+++ KarpathySplit.py
@@ -1,56 +1,105 @@
-
-# coding: utf-8
-
-# # Karpathy Split for MS-COCO Dataset
 import json
-from random import shuffle, seed
+from os import makedirs
+from os.path import join, isdir
+import argparse
 
-seed( 123 ) # Make it reproducible
+def main(args):
 
-num_val = 5000
-num_test = 5000
+    # make output dir if it doesn't exist yet
+    if not isdir(args.out_dir):
+        makedirs(args.out_dir)
+        print('created output directory: {}'.format(args.out_dir))
 
-val = json.load( open('annotations/captions_val2014.json', 'r') )
-train = json.load( open('annotations/captions_train2014.json', 'r') )
+    print('load Karpathy splits...')
+    with open(args.karpathy_path) as file:
+        k_data = json.load(file)['images']
 
-# Merge together
-imgs = val['images'] + train['images']
-annots = val['annotations'] + train['annotations']
+    # get corresponding ids for splits
+    train_ids = [i['cocoid'] for i in k_data if i['split'] == 'train']
+    restval_ids = [i['cocoid'] for i in k_data if i['split'] == 'restval']
+    train_ids = train_ids + restval_ids  # add restval ids to train_ids
+    val_ids = [i['cocoid'] for i in k_data if i['split'] == 'val']
+    test_ids = [i['cocoid'] for i in k_data if i['split'] == 'test']
 
-shuffle( imgs )
+    print('# images: \ntrain: {}, val: {}, test: {}'.format(
+            len(train_ids), len(val_ids), len(test_ids)
+        ))
 
-# Split into val, test, train
-dataset = {}
-dataset[ 'val' ] = imgs[ :num_val ]
-dataset[ 'test' ] = imgs[ num_val: num_val + num_test ]
-dataset[ 'train' ] = imgs[ num_val + num_test: ]
+    print('load coco data...')
+    val = json.load(
+        open(join(args.coco_dir, 'captions_val2014.json'), 'r')
+        )
+    train = json.load(
+        open(join(args.coco_dir, 'captions_train2014.json'), 'r')
+        )
 
-# Group by image ids
-itoa = {}
-for a in annots:
-    imgid = a['image_id']
-    if not imgid in itoa: itoa[imgid] = []
-    itoa[imgid].append(a)
+    # Merge together
+    imgs = val['images'] + train['images']
+    annots = val['annotations'] + train['annotations']
+
+    print('split images into val, test, train...')
+    dataset = {}
+    dataset['val'] = [i for i in imgs if i['id'] in val_ids]
+    dataset['test'] = [i for i in imgs if i['id'] in test_ids]
+    dataset['train'] = [i for i in imgs if i['id'] in train_ids]
+
+    # setup json data
+    json_data = {}
+    info = train['info']
+    licenses = train['licenses']
+
+    # Group annotations by image ids
+    itoa = {}
+    for a in annots:
+        imgid = a['image_id']
+        if imgid not in itoa:
+            itoa[imgid] = []
+        itoa[imgid].append(a)
+
+    for subset in ['val', 'test', 'train']:
+
+        json_data[subset] = {
+            'type': 'caption', 'info': info, 'licenses': licenses,
+            'images': [], 'annotations': []
+            }
+
+        for img in dataset[subset]:
+
+            img_id = img['id']
+            anns = itoa[img_id]
+
+            json_data[subset]['images'].append(img)
+            json_data[subset]['annotations'].extend(anns)
+
+        print('{} split: {} images | {} captions'.format(
+            subset,
+            len(json_data[subset]['images']),
+            len(json_data[subset]['annotations'])
+            ))
+
+        out_path = join(args.out_dir, 'karpathy_split_'+subset+'.json')
+        json.dump(json_data[subset], open(out_path, 'w'))
+        print('saved to file', out_path)
 
 
-json_data = {}
-info = train['info']
-licenses = train['licenses']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f', default='self', help='To make it runnable in jupyter'
+        )
+    parser.add_argument(
+        '--coco_dir', type=str, help='path to coco annotations'
+        )
+    parser.add_argument(
+        '--karpathy_path', type=str, help='path to Karpathy split file',
+        default='./data/coco_splits.json'
+        )
 
-split = [ 'val', 'test', 'train' ]
+    parser.add_argument(
+        '--out_dir', type=str, help='directory for output data',
+        default='./annotations'
+    )
 
-for subset in split:
-    
-    json_data[ subset ] = { 'type':'caption', 'info':info, 'licenses': licenses,
-                           'images':[], 'annotations':[] }
-    
-    for img in dataset[ subset ]:
-        
-        img_id = img['id']
-        anns = itoa[ img_id ]
-        
-        json_data[ subset ]['images'].append( img )
-        json_data[ subset ]['annotations'].extend( anns )
-        
-    json.dump( json_data[ subset ], open( 'annotations/karpathy_split_' + subset + '.json', 'w' ) )
+    args = parser.parse_args()
 
+    main(args)
--- data/download.sh
+++ data/download.sh
@@ -1,10 +1,4 @@
-wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip -P ./
-wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip -P ./
-wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip -P ./
+wget https://cs.stanford.edu/people/karpathy/deepimagesent/caption_datasets.zip ./
 
-unzip ./captions_train-val2014.zip -d ./
-rm ./captions_train-val2014.zip
-unzip ./train2014.zip -d ./
-rm ./train2014.zip 
-unzip ./val2014.zip -d ./
-rm ./val2014.zip 
+unzip -p ./caption_datasets.zip dataset_coco.json > ./coco_splits.json
+rm ./caption_datasets.zip
