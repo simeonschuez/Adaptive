f0cc70d4c3456f97376e3d528768b049fa763ea2
Simeon Schüz <simeon.schuez@uni-jena.de>
2020-10-13 17:56:55 +0200

    replaced deprecated functions

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,7 @@ import os
 import pickle
 import string
 import numpy as np
-import nltk
+from nltk.tokenize import word_tokenize
 from PIL import Image
 from build_vocab import Vocabulary
 from coco.pycocotools.coco import COCO
@@ -15,7 +15,7 @@ class CocoDataset(data.Dataset):
     """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
     def __init__(self, root, json, vocab, transform=None):
         """Set the path for images, captions and vocabulary wrapper.
-        
+
         Args:
             root: image directory.
             json: coco annotation file path.
@@ -48,7 +48,9 @@ class CocoDataset(data.Dataset):
             image = self.transform( image )
 
         # Convert caption (string) to word ids.
-        tokens = str( caption ).lower().translate( None, string.punctuation ).strip().split()
+        tokens = word_tokenize(caption.lower())
+        tokens = [token for token in tokens if token not in string.punctuation]
+
         caption = []
         caption.append(vocab('<start>'))
         caption.extend([vocab(token) for token in tokens])
@@ -62,12 +64,12 @@ class CocoDataset(data.Dataset):
 
 def collate_fn(data):
     """Creates mini-batch tensors from the list of tuples (image, caption).
-    
-    We should build custom collate_fn rather than using default collate_fn, 
+
+    We should build custom collate_fn rather than using default collate_fn,
     because merging caption (including padding) is not supported in default.
 
     Args:
-        data: list of tuple (image, caption). 
+        data: list of tuple (image, caption).
             - image: torch tensor of shape (3, 256, 256).
             - caption: torch tensor of shape (?); variable length.
 
@@ -93,7 +95,7 @@ def collate_fn(data):
     targets = torch.zeros(len(captions), max(lengths)).long()
     for i, cap in enumerate(captions):
         end = lengths[i]
-        targets[i, :end] = cap[:end]        
+        targets[i, :end] = cap[:end]
     return images, targets, lengths, img_ids, filenames
 
 
@@ -104,13 +106,13 @@ def get_loader(root, json, vocab, transform, batch_size, shuffle, num_workers):
                        json=json,
                        vocab=vocab,
                        transform=transform)
-    
+
     # Data loader for COCO dataset
     # This will return (images, captions, lengths) for every iteration.
     # images: tensor of shape (batch_size, 3, 224, 224).
     # captions: tensor of shape (batch_size, padded_length).
     # lengths: list indicating valid length for each caption. length is (batch_size).
-    data_loader = torch.utils.data.DataLoader(dataset=coco, 
+    data_loader = torch.utils.data.DataLoader(dataset=coco,
                                               batch_size=batch_size,
                                               shuffle=shuffle,
                                               num_workers=num_workers,
