8273cc82f6789b18a69c0b192de214b282d569f4
Simeon Schüz <simeon.schuez@uni-jena.de>
2020-12-18 10:09:59 +0100

    fixed wrong path in bash scripts

--- train_caps.sh
+++ train_caps.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 vocab_path='./data/coco_vocab.pkl'
 image_dir='./data/images/mscoco/'
@@ -7,15 +8,4 @@ batch_size=60
 eval_size=28
 model_name='adaptive-captions'
 
-cd ..
-
-python train_caption.py
-  --vocab_path $vocab_path
-  --image_dir $image_dir
-  --caption_path $caption_path
-  --splits_path $splits_path
-  --batch_size $batch_size
-  --eval_size $eval_size
-  --model_name $model_name
-
-cd bash
+python train_caption.py --vocab_path $vocab_path --image_dir $image_dir --caption_path $caption_path --splits_path $splits_path --batch_size $batch_size --eval_size $eval_size --model_name $model_name
--- train_refcocoplus.sh
+++ train_refcocoplus.sh
@@ -1,19 +1,10 @@
+#!/bin/bash
 
-vocab_path='./data/refcoco_vocab.pkl'
+vocab_path='./data/refcocoplus_vocab.pkl'
 image_dir='./data/images/mscoco/'
 refcoco_path='./data/refcoco+/'
 batch_size=60
 eval_size=28
 model_name='adaptive-refcocoplus'
 
-cd ..
-
-python train_reg.py
-  --vocab_path $vocab_path
-  --image_dir $image_dir
-  --refcoco_path $refcoco_path
-  --batch_size $batch_size
-  --eval_size $eval_size
-  --model_name $model_name
-
-cd bash
+python train_reg.py --vocab_path $vocab_path --image_dir $image_dir --refcoco_path $refcoco_path --batch_size $batch_size --eval_size $eval_size --model_name $model_name
